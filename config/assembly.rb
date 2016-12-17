a561a2df9eb25a452c9ea18836f516b298d56f2c
Matt Edlefsen <matt.edlefsen@gmail.com>
2016-12-17 13:17:05 -0800

    Version 0.4

--- config/assembly.rb
+++ config/assembly.rb
@@ -1,21 +1,15 @@
 Alki do
   set :name, 'app'
-  set :default_root do
-    root
+  set :root_group do
+    parent
   end
 
   func :run do
-    root_console.start
+    console.start
   end
 
-  factory :console do
+  service :console do
     require 'alki/console/pry_console'
-    -> (name,root) {
-      Alki::Console::PryConsole.new name, root
-    }
-  end
-
-  service :root_console do
-    console name, default_root
+    Alki::Console::PryConsole.new name, root_group
   end
 end
