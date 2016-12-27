fd41312218b54e777a7fc4d9e5a36620a76271ae
Matt Edlefsen <matt.edlefsen@gmail.com>
2016-12-26 18:34:03 -0800

    Version 0.5.0
    Bump to alki 0.10.0

--- config/assembly.rb
+++ config/assembly.rb
@@ -1,13 +1,14 @@
 Alki do
   set :name, 'app'
-  set :root_group do
-    parent
-  end
-
   func :run do
     console.start
   end
 
+  set :root_group do
+    parent
+  end
+
+  tag :main_loop
   service :console do
     require 'alki/console/pry_console'
     Alki::Console::PryConsole.new name, root_group
